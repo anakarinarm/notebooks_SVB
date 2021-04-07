5b14b5ba74f3f6d256bab77af2607ae132b5fda6
Karina Ramos <kramosmu@eoas.ubc.ca>
2021-04-07 10:24:26 -0500

    adding notebooks generate T and S ini conditions from CASE-STSE output

--- ssh_tools.py
+++ ssh_tools.py
@@ -7,61 +7,62 @@ import pandas as pd
 from scipy.signal import butter, filtfilt
 
 
-def get_redmar_dataframe(filename):
+def get_redmar_dataframe(filename, na_val=9999):
     '''Get a dataframe from a REDMAR data file indexed by date '''
     col_names = ['year','month', 'day', 'hour','minute','second','ID',
                 'voltage','ssh_leveltrol','ssh_burbujeador',
                 'switch1','switch2', 'water_temp','ssh_radar',
                 'solar_radiation','wind_dir','wind_speed',
-                'air_temp','rel_humidity','atm_pressure', 'precipitation', 
+                'air_temp','rel_humidity','atm_pressure', 'precipitation',
                 'voltage_station', 'ssh_radar_sutron']
-    data = pd.read_csv(filename, delim_whitespace = True, names = col_names )
+    data = pd.read_csv(filename, delim_whitespace = True, names = col_names,
+                       na_values=na_val)
     data['datetime'] = pd.to_datetime(data[['year', 'month', 'day','hour','minute','second']])
     return(data.set_index('datetime'))
 
 def filter_timeseries(record, winlen=39, method='box'):
-    """Filter a timeseries - Function from SalishSeaTools tidetools.py 
+    """Filter a timeseries - Function from SalishSeaTools tidetools.py
     (https://github.com/SalishSeaCast/tools/blob/master/SalishSeaTools/salishsea_tools/tidetools.py)
-    
+
     IF DOODSON: USE WITH 1H RECORDS ONLY
-    
+
     Developed for wind and tidal filtering, but can be modified for use
     with a variety of timeseries data. The data record should be at least
     half a window length longer at either end than the period of interest
     to accommodate window length shrinking near the array edges.
     *This function can only operate along the 0 axis. Please modify to include
     an axis argument in the future.*
-    
+
     Types of filters (*please add to these*):
     * **box**: simple running mean
     * **doodson**: Doodson bandpass filter (39 winlen required)
-    
+
     :arg record: timeseries record to be filtered
     :type record: :py:class:`numpy.ndarray`, :py:class:`xarray.DataArray`,
                   or :py:class:`netCDF4.Variable`
-    
+
     :arg winlen: window length
     :type winlen: integer
-    
+
     :arg method: type of filter (ex. 'box', 'doodson', etc.)
     :type method: string
-    
+
     :returns filtered: filtered timeseries
     :rtype: same as record
     """
-    
+
     # Preallocate filtered record
     filtered = record.copy()
-    
+
     # Length along time axis
     record_length = record.shape[0]
 
     # Window length
     w = (winlen - 1) // 2
-    
+
     # Construct weight vector
     weight = np.zeros(w, dtype=int)
-    
+
     # Select filter method
     if method == 'doodson':
         # Doodson bandpass filter (winlen must be 39)
@@ -74,27 +75,27 @@ def filter_timeseries(record, winlen=39, method='box'):
         centerval = 1
     else:
         raise ValueError('Invalid filter method: {}'.format(method))
-    
+
     # Loop through record
     for i in range(record_length):
-        
+
         # Adjust window length for end cases
         W = min(i, w, record_length-i-1)
         Weight = weight[:W]
         Weight = np.append(Weight[::-1], np.append(centerval, Weight))
         if sum(Weight) != 0:
             Weight = (Weight/sum(Weight))
-        
+
         # Expand weight dims so it can operate on record window
         for dim in range(record.ndim - 1):
             Weight = Weight[:, np.newaxis]
-        
+
         # Apply mean over window length
         if W > 0:
             filtered[i, ...] = np.sum(record[i-W:i+W+1, ...] * Weight, axis=0)
         else:
             filtered[i, ...] = record[i, ...]
-    
+
     return filtered
 
 def butter_lowpass(lowcut, fs, order=5):
@@ -105,7 +106,7 @@ def butter_lowpass(lowcut, fs, order=5):
        OUTPUT
        b:
        a:
-       '''    
+       '''
     nyq = 0.5 * fs
     low = lowcut / nyq
     b, a = butter(order, low, btype='low')
@@ -120,7 +121,7 @@ def butter_lowpass_filter(data, lowcut, fs, order=5):
        order::int, ortder of the filter
        OUTPUT
        y::array, filtered signal of same size as data
-       '''   
+       '''
     b, a = butter_lowpass(lowcut, fs, order=order)
     y = filtfilt(b, a, data)
     return y
@@ -133,7 +134,7 @@ def butter_highpass(highcut, fs, order=5):
        OUTPUT
        b:
        a:
-       '''    
+       '''
     nyq = 0.5 * fs
     high = highcut / nyq
     b, a = butter(order, high, btype='high')
@@ -148,7 +149,7 @@ def butter_highpass_filter(data, highcut, fs, order=5):
        order::int, ortder of the filter
        OUTPUT
        y::array, filtered signal of same size as data
-       '''   
+       '''
     b, a = butter_highpass(highcut, fs, order=order)
     y = filtfilt(b, a, data)
     return y
